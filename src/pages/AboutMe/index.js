0ffb3e9b0be7b583ae2d692ba3441b183c784f58
seandepottey <seandepottey@gmail.com>
2021-09-03 13:49:52 -0500

    About Me page - update
    - Began building node structure
    - Filled in images for the personal section as well ravens section

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
+// #region imports
+// Components
+import BasicContent from '../../components/BasicContent';
+
+// Media
+import Selfie from '../../assets/profile-pic-small.jpg';
+import Raven from '../../assets/raven.jpg';
+// #endregion imports
+
 const AboutMePage = () => {
     return (
-        <div>About Page</div>
+        <div id='about-me-page'>
+            <section id='personal-section'>
+                <BasicContent
+                    imageSrc={Selfie}
+                >
+
+                </BasicContent>
+            </section>
+            <section id='raven-section'>
+                <BasicContent
+                    flip
+                    imageSrc={Raven}
+                >
+
+                </BasicContent>
+            </section>
+        </div>
     );
 };
 
