10ef94914eaf9f4d7732241537f3a43fbca2988e
seandepottey <seandepottey@gmail.com>
2021-09-07 10:48:33 -0500

    Applicant Title update
    - Switched the ordering on desktop of the image and text.
    - Made image inline with right hand column for desktop.

--- src/components/Applicant Title/index.js
+++ src/components/Applicant Title/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // #region imports
 //Components
-import { Image } from 'react-bootstrap';
+import { Row, Col, Image } from 'react-bootstrap';
 // #endregion imports
 
 // #region component
@@ -13,15 +13,17 @@ const ApplicantTitle = ({
     textColor
 }) => {
     return (
-        <div className='Applicant-title d-flex align-items-center pb-4'>
-            <div className='Applicant-image-wrapper d-flex align-items-center justify-content-center shadow'>
-                <Image className='Applicant-image' fluid roundedCircle src={profilePic} />
-            </div>
-            <div className='ps-5'>
+        <Row className='Applicant-title align-items-center justify-content-lg-center pb-4'>
+            <Col md={{span: 4, offset: 1, order: 2}} lg={{span: 3, offset: 1}}>
+                <div className='Applicant-image-wrapper d-flex align-items-center justify-content-center mb-2 mb-md-0 shadow'>
+                    <Image className='Applicant-image' fluid roundedCircle src={profilePic} />
+                </div>
+            </Col>
+            <Col md={{span: 7, order: 1}} lg={6}>
                 <h1 className='fw-bold text-uppercase'>{name}</h1>
                 <h4 className={`${textColor}`}>{jobTitle}</h4>
-            </div>
-        </div>
+            </Col>
+        </Row>
     );
 };
 
