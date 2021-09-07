2780975a538e330eee707ed18a65b8ef91e592a0
seandepottey <seandepottey@gmail.com>
2021-09-07 10:47:27 -0500

    Skills Section component
    - Built out node structure
    - Added font-size styling

--- src/components/SkillsSection/index.js
+++ src/components/SkillsSection/index.js
@@ -1,22 +1,35 @@
 import React from 'react';
 
 const SkillsSection = ({
+    content,
     content: {
         header,
         value
     }
 }) => {
+    let skillsList = <p>{content.value}</p>;
+    const createContentNodes = (item) => {
+        console.log(item);
+        if(item.subHeader) {
+            let subheader = <h6 className='mb-1 text-secondary'>{item.subHeader}</h6>;
+            skillsList.push(subheader);
+        }
+        let listNode = <p className=''>{item.value}</p>;
+        skillsList.push(listNode);
+    };
+
     const detectArray = Array.isArray(value);
     if(detectArray) {
-        value.forEach(
-            
-        );
+        skillsList = [];
+        value.forEach(item => createContentNodes(item));
     }
 
     return (
-        <div className='SkillsSection'>
+        <div className='SkillsSection mb-5'>
             <h3 className='mb-3'>{header}</h3>
-            <p></p>
+            <div className='SkillsList fst-italic'>
+                {skillsList}
+            </div>
         </div>
     );
 };
