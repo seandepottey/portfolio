2771710d535034a8141bf07b624639e1e774fe6f
seandepottey <seandepottey@gmail.com>
2021-09-07 10:45:16 -0500

    Education Component

---
+++ src/components/Education/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const SchoolInformation = ({
+    schoolName,
+    degree,
+    focus,
+    years,
+    link
+}) => {
+
+    if(link) {
+        schoolName = <a target='_blank' href={link}>{schoolName}</a>;
+    }
+    return(
+        <div className='SchoolInformation'>
+            <p>
+                <span>{schoolName}</span>
+                <span>{degree}:</span>
+                <span>{focus}</span>
+                <span className='years'>{years}</span>
+            </p>
+        </div>
+    );
+}
+
+const EducationSection = ({content}) => {
+    let schools = [];
+    console.log(content);
+    content.forEach(school => schools.push(SchoolInformation(school)));
+
+    return (
+        <div id='EducationSection'>
+            <h2>Education</h2>
+            {schools}        
+        </div>
+    );
+};
+
+export default EducationSection;
